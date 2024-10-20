a06aecb369ad79988848f3d5984b39a550bec950
stenin-nikita <stenin.nikita@gmail.com>
2024-10-20 03:12:36 +0300

    test: add tests for match method

--- src/__test__/path-pattern.test.ts
+++ src/__test__/path-pattern.test.ts
@@ -3,19 +3,157 @@ import { describe, expect, it } from 'vitest';
 import { PathPattern } from '../path-pattern';
 
 describe('PathPattern', () => {
-  it('extract last param with same name', () => {
-    const pattern = new PathPattern('/{page}/{id}/{id}');
+  describe('match', () => {
+    it(`should match '' with input '/'`, () => {
+      const pattern = new PathPattern('');
 
-    const result = pattern.match('/users/12/34');
+      const result = pattern.match('/');
 
-    expect(result).toEqual({ page: 'users', id: '34' });
-  });
+      expect(result).toEqual({});
+    });
 
-  it('extract repeatable params', () => {
-    const pattern = new PathPattern('/{name}+');
+    it(`should match '/' with input '/'`, () => {
+      const pattern = new PathPattern('/');
 
-    const result = pattern.match('/12/34');
+      const result = pattern.match('/');
 
-    expect(result).toEqual({ name: ['12', '34'] });
+      expect(result).toEqual({});
+    });
+
+    it(`should not match '' with input ''`, () => {
+      const pattern = new PathPattern('');
+
+      const result = pattern.match('');
+
+      expect(result).toEqual(null);
+    });
+
+    it(`should not match '/' with input ''`, () => {
+      const pattern = new PathPattern('/');
+
+      const result = pattern.match('');
+
+      expect(result).toEqual(null);
+    });
+
+    it(`should not match '/' with input '/home'`, () => {
+      const pattern = new PathPattern('/');
+
+      const result = pattern.match('/home');
+
+      expect(result).toEqual(null);
+    });
+
+    it(`should not match '/home' with input '/'`, () => {
+      const pattern = new PathPattern('/home');
+
+      const result = pattern.match('/');
+
+      expect(result).toEqual(null);
+    });
+
+    it(`should match '/home' with input '/home'`, () => {
+      const pattern = new PathPattern('/home');
+
+      const result = pattern.match('/home');
+
+      expect(result).toEqual({});
+    });
+
+    it(`should not match '/home' with input '/index'`, () => {
+      const pattern = new PathPattern('/home');
+
+      const result = pattern.match('/index');
+
+      expect(result).toEqual(null);
+    });
+
+    it(`should not match '/home' with input '/home/page'`, () => {
+      const pattern = new PathPattern('/home');
+
+      const result = pattern.match('/home/page');
+
+      expect(result).toEqual(null);
+    });
+
+    it(`should not match '/home' with input '/home/'`, () => {
+      const pattern = new PathPattern('/home');
+
+      const result = pattern.match('/home/');
+
+      expect(result).toEqual(null);
+    });
+
+    it(`should not match '/home' with input '/Home'`, () => {
+      const pattern = new PathPattern('/home');
+
+      const result = pattern.match('/Home');
+
+      expect(result).toEqual(null);
+    });
+
+    it(`should match '/home' with ignoreCase and input '/Home'`, () => {
+      const pattern = new PathPattern('/home', { ignoreCase: true });
+
+      const result = pattern.match('/Home');
+
+      expect(result).toEqual({});
+    });
+
+    it(`should not match '/home/' with input '/home'`, () => {
+      const pattern = new PathPattern('/home/');
+
+      const result = pattern.match('/home');
+
+      expect(result).toEqual(null);
+    });
+
+    it(`should match '/home/' with input '/home/'`, () => {
+      const pattern = new PathPattern('/home/');
+
+      const result = pattern.match('/home/');
+
+      expect(result).toEqual({});
+    });
+
+    it(`should not match '/{param}' with input '/'`, () => {
+      const pattern = new PathPattern('/{param}');
+
+      const result = pattern.match('/');
+
+      expect(result).toEqual(null);
+    });
+
+    it(`should match '/{param}' with input '/about'`, () => {
+      const pattern = new PathPattern('/{param}');
+
+      const result = pattern.match('/about');
+
+      expect(result).toEqual({ param: 'about' });
+    });
+
+    it(`should not match '/{param}' with input '/about/'`, () => {
+      const pattern = new PathPattern('/{param}');
+
+      const result = pattern.match('/about/');
+
+      expect(result).toEqual(null);
+    });
+
+    it('should extract last param with same name', () => {
+      const pattern = new PathPattern('/{page}/{id}/{id}');
+
+      const result = pattern.match('/users/12/34');
+
+      expect(result).toEqual({ page: 'users', id: '34' });
+    });
+
+    it('should extract repeatable params', () => {
+      const pattern = new PathPattern('/{name}+');
+
+      const result = pattern.match('/12/34');
+
+      expect(result).toEqual({ name: ['12', '34'] });
+    });
   });
 });
