ce258d6a8c2a808c657a99156c1b8a597da31d07
stenin-nikita <stenin.nikita@gmail.com>
2024-10-27 18:28:09 +0300

    test: update tests

--- src/__test__/path-pattern.test.ts
+++ src/__test__/path-pattern.test.ts
@@ -4,348 +4,392 @@ import { PathPattern } from '../path-pattern';
 
 describe('PathPattern', () => {
   describe('match', () => {
-    it(`should match '' with input '/'`, () => {
+    describe('empty', () => {
       const pattern = new PathPattern('');
 
-      const result = pattern.exec('/');
+      it(`should match with input ''`, () => {
+        const result = pattern.exec('');
 
-      expect(result).toEqual({});
+        expect(result).toEqual({});
+      });
+
+      it(`should match with input '/'`, () => {
+        const result = pattern.exec('/');
+
+        expect(result).toEqual({});
+      });
     });
 
-    it(`should match '/' with input '/'`, () => {
+    describe('/', () => {
       const pattern = new PathPattern('/');
 
-      const result = pattern.exec('/');
+      it(`should match with input ''`, () => {
+        const result = pattern.exec('');
 
-      expect(result).toEqual({});
+        expect(result).toEqual({});
+      });
+
+      it(`should match with input '/'`, () => {
+        const result = pattern.exec('/');
+
+        expect(result).toEqual({});
+      });
+
+      it(`should not match with input '/home'`, () => {
+        const result = pattern.exec('/home');
+
+        expect(result).toEqual(null);
+      });
     });
 
-    it(`should not match '' with input ''`, () => {
-      const pattern = new PathPattern('');
-
-      const result = pattern.exec('');
-
-      expect(result).toEqual(null);
-    });
-
-    it(`should not match '/' with input ''`, () => {
-      const pattern = new PathPattern('/');
-
-      const result = pattern.exec('');
-
-      expect(result).toEqual(null);
-    });
-
-    it(`should not match '/' with input '/home'`, () => {
-      const pattern = new PathPattern('/');
-
-      const result = pattern.exec('/home');
-
-      expect(result).toEqual(null);
-    });
-
-    it(`should not match '/home' with input '/'`, () => {
+    describe('/home', () => {
       const pattern = new PathPattern('/home');
 
-      const result = pattern.exec('/');
+      it(`should not match with input '/'`, () => {
+        const result = pattern.exec('/');
 
-      expect(result).toEqual(null);
+        expect(result).toEqual(null);
+      });
+
+      it(`should match with input '/home'`, () => {
+        const result = pattern.exec('/home');
+
+        expect(result).toEqual({});
+      });
+
+      it(`should not match with input '/index'`, () => {
+        const result = pattern.exec('/index');
+
+        expect(result).toEqual(null);
+      });
+
+      it(`should not match with input '/home/page'`, () => {
+        const result = pattern.exec('/home/page');
+
+        expect(result).toEqual(null);
+      });
+
+      it(`should not match with input '/home/'`, () => {
+        const result = pattern.exec('/home/');
+
+        expect(result).toEqual(null);
+      });
+
+      it(`should not match with input '/Home'`, () => {
+        const result = pattern.exec('/Home');
+
+        expect(result).toEqual(null);
+      });
+
+      it(`should match with ignoreCase and input '/Home'`, () => {
+        const pattern = new PathPattern('/home', { ignoreCase: true });
+
+        const result = pattern.exec('/Home');
+
+        expect(result).toEqual({});
+      });
     });
 
-    it(`should match '/home' with input '/home'`, () => {
-      const pattern = new PathPattern('/home');
-
-      const result = pattern.exec('/home');
-
-      expect(result).toEqual({});
-    });
-
-    it(`should not match '/home' with input '/index'`, () => {
-      const pattern = new PathPattern('/home');
-
-      const result = pattern.exec('/index');
-
-      expect(result).toEqual(null);
-    });
-
-    it(`should not match '/home' with input '/home/page'`, () => {
-      const pattern = new PathPattern('/home');
-
-      const result = pattern.exec('/home/page');
-
-      expect(result).toEqual(null);
-    });
-
-    it(`should not match '/home' with input '/home/'`, () => {
-      const pattern = new PathPattern('/home');
-
-      const result = pattern.exec('/home/');
-
-      expect(result).toEqual(null);
-    });
-
-    it(`should not match '/home' with input '/Home'`, () => {
-      const pattern = new PathPattern('/home');
-
-      const result = pattern.exec('/Home');
-
-      expect(result).toEqual(null);
-    });
-
-    it(`should match '/home' with ignoreCase and input '/Home'`, () => {
-      const pattern = new PathPattern('/home', { ignoreCase: true });
-
-      const result = pattern.exec('/Home');
-
-      expect(result).toEqual({});
-    });
-
-    it(`should not match '/home/' with input '/home'`, () => {
+    describe('/home/', () => {
       const pattern = new PathPattern('/home/');
 
-      const result = pattern.exec('/home');
+      it(`should not match with input '/home'`, () => {
+        const result = pattern.exec('/home');
 
-      expect(result).toEqual(null);
+        expect(result).toEqual(null);
+      });
+
+      it(`should match with input '/home/'`, () => {
+        const result = pattern.exec('/home/');
+
+        expect(result).toEqual({});
+      });
     });
 
-    it(`should match '/home/' with input '/home/'`, () => {
-      const pattern = new PathPattern('/home/');
-
-      const result = pattern.exec('/home/');
-
-      expect(result).toEqual({});
-    });
-
-    it(`should not match '/{param}' with input '/'`, () => {
+    describe('/{param}', () => {
       const pattern = new PathPattern('/{param}');
 
-      const result = pattern.exec('/');
+      it(`should not match with input '/'`, () => {
+        const result = pattern.exec('/');
 
-      expect(result).toEqual(null);
+        expect(result).toEqual(null);
+      });
+
+      it(`should match with input '/about'`, () => {
+        const result = pattern.exec('/about');
+
+        expect(result).toEqual({ param: 'about' });
+      });
+
+      it(`should not match with input '/about/'`, () => {
+        const result = pattern.exec('/about/');
+
+        expect(result).toEqual(null);
+      });
     });
 
-    it(`should match '/{param}' with input '/about'`, () => {
-      const pattern = new PathPattern('/{param}');
-
-      const result = pattern.exec('/about');
-
-      expect(result).toEqual({ param: 'about' });
-    });
-
-    it(`should not match '/{param}' with input '/about/'`, () => {
-      const pattern = new PathPattern('/{param}');
-
-      const result = pattern.exec('/about/');
-
-      expect(result).toEqual(null);
-    });
-
-    it(`should match '/{param}?' with input ''`, () => {
+    describe('/{param}?', () => {
       const pattern = new PathPattern('/{param}?');
 
-      const result = pattern.exec('');
+      it(`should match with input ''`, () => {
+        const result = pattern.exec('');
 
-      expect(result).toEqual({ param: '' });
+        expect(result).toEqual({ param: '' });
+      });
+
+      it(`should match with input '/'`, () => {
+        const result = pattern.exec('/');
+
+        expect(result).toEqual({ param: '' });
+      });
+
+      it(`should match with input '/users'`, () => {
+        const result = pattern.exec('/users');
+
+        expect(result).toEqual({ param: 'users' });
+      });
+
+      it(`should not match with input '/users/123'`, () => {
+        const result = pattern.exec('/users/123');
+
+        expect(result).toEqual(null);
+      });
     });
 
-    it.skip(`should match '/{param}?' with input '/'`, () => {
-      const pattern = new PathPattern('/{param}?');
-
-      const result = pattern.exec('/');
-
-      expect(result).toEqual({ param: '' });
-    });
-
-    it(`should match '/{param}?' with input '/users'`, () => {
-      const pattern = new PathPattern('/{param}?');
-
-      const result = pattern.exec('/users');
-
-      expect(result).toEqual({ param: 'users' });
-    });
-
-    it(`should not match '/{param}?' with input '/users/123'`, () => {
-      const pattern = new PathPattern('/{param}?');
-
-      const result = pattern.exec('/users/123');
-
-      expect(result).toEqual(null);
-    });
-
-    it.skip(`should match '/users/{id}?' with input '/users/'`, () => {
+    describe('/users/{id}?', () => {
       const pattern = new PathPattern('/users/{id}?');
 
-      const result = pattern.exec('/users/');
+      it(`should not match with input '/users/123/'`, () => {
+        const result = pattern.exec('/users/123/');
 
-      expect(result).toEqual({ id: '' });
+        expect(result).toEqual(null);
+      });
+
+      it(`should match with input '/users/123'`, () => {
+        const result = pattern.exec('/users/123');
+
+        expect(result).toEqual({ id: '123' });
+      });
+
+      it(`should not match with input '/users//'`, () => {
+        const result = pattern.exec('/users//');
+
+        expect(result).toEqual(null);
+      });
+
+      it(`should match with input '/users/'`, () => {
+        const result = pattern.exec('/users/');
+
+        expect(result).toEqual({ id: '' });
+      });
+
+      it(`should match with input '/users'`, () => {
+        const result = pattern.exec('/users');
+
+        expect(result).toEqual({ id: '' });
+      });
     });
 
-    it(`should match '/users/{id}?' with input '/users'`, () => {
-      const pattern = new PathPattern('/users/{id}?');
+    describe('/users/{id}?/', () => {
+      const pattern = new PathPattern('/users/{id}?/');
 
-      const result = pattern.exec('/users');
+      it(`should match with input '/users/123/'`, () => {
+        const result = pattern.exec('/users/123/');
 
-      expect(result).toEqual({ id: '' });
+        expect(result).toEqual({ id: '123' });
+      });
+
+      it(`should not match with input '/users/123'`, () => {
+        const result = pattern.exec('/users/123');
+
+        expect(result).toEqual(null);
+      });
+
+      it(`should match with input '/users//'`, () => {
+        const result = pattern.exec('/users//');
+
+        expect(result).toEqual({ id: '' });
+      });
+
+      it(`should match with input '/users/'`, () => {
+        const result = pattern.exec('/users/');
+
+        expect(result).toEqual({ id: '' });
+      });
+
+      it(`should not match with input '/users'`, () => {
+        const result = pattern.exec('/users');
+
+        expect(result).toEqual(null);
+      });
     });
 
-    it(`should match '/users/{id}?' with input '/users/123'`, () => {
-      const pattern = new PathPattern('/users/{id}?');
-
-      const result = pattern.exec('/users/123');
-
-      expect(result).toEqual({ id: '123' });
-    });
-
-    it(`should match '/{locale}?/users' with input '/users'`, () => {
+    describe('/{locale}?/users', () => {
       const pattern = new PathPattern('/{locale}?/users');
 
-      const result = pattern.exec('/users');
+      it(`should match with input '/users'`, () => {
+        const result = pattern.exec('/users');
 
-      expect(result).toEqual({ locale: '' });
+        expect(result).toEqual({ locale: '' });
+      });
+
+      it(`should match with input '/en/users'`, () => {
+        const result = pattern.exec('/en/users');
+
+        expect(result).toEqual({ locale: 'en' });
+      });
     });
 
-    it(`should match '/{locale}?/users' with input '/en/users'`, () => {
-      const pattern = new PathPattern('/{locale}?/users');
-
-      const result = pattern.exec('/en/users');
-
-      expect(result).toEqual({ locale: 'en' });
-    });
-
-    it(`should match '/file/{path}*' with input '/file'`, () => {
+    describe('/file/{path}*', () => {
       const pattern = new PathPattern('/file/{path}*');
 
-      const result = pattern.exec('/file');
+      it(`should match with input '/file'`, () => {
+        const result = pattern.exec('/file');
 
-      expect(result).toEqual({ path: [''] });
+        expect(result).toEqual({ path: [''] });
+      });
+
+      it(`should match with input '/file/'`, () => {
+        const result = pattern.exec('/file/');
+
+        expect(result).toEqual({ path: [''] });
+      });
+
+      it(`should match with input '/file/a'`, () => {
+        const result = pattern.exec('/file/a');
+
+        expect(result).toEqual({ path: ['a'] });
+      });
+
+      it(`should match with input '/file/a/b/'`, () => {
+        const result = pattern.exec('/file/a/b/');
+
+        expect(result).toEqual({ path: ['a', 'b', ''] });
+      });
+
+      it(`should match with input '/file/a/b/c'`, () => {
+        const result = pattern.exec('/file/a/b/c');
+
+        expect(result).toEqual({ path: ['a', 'b', 'c'] });
+      });
     });
 
-    it(`should match '/file/{path}*' with input '/file/a'`, () => {
-      const pattern = new PathPattern('/file/{path}*');
-
-      const result = pattern.exec('/file/a');
-
-      expect(result).toEqual({ path: ['a'] });
-    });
-
-    it.skip(`should match '/{path}*' with input '/a/b/'`, () => {
-      const pattern = new PathPattern('/{path}*');
-
-      const result = pattern.exec('/a/b/');
-
-      expect(result).toEqual({ path: ['a', 'b', ''] });
-    });
-
-    it(`should match '/file/{path}*' with input '/file/a/b/c'`, () => {
-      const pattern = new PathPattern('/file/{path}*');
-
-      const result = pattern.exec('/file/a/b/c');
-
-      expect(result).toEqual({ path: ['a', 'b', 'c'] });
-    });
-
-    it(`should match '/{path}+' with input '/a'`, () => {
+    describe('/{path}+', () => {
       const pattern = new PathPattern('/{path}+');
 
-      const result = pattern.exec('/a');
+      it(`should match with input '/a'`, () => {
+        const result = pattern.exec('/a');
 
-      expect(result).toEqual({ path: ['a'] });
+        expect(result).toEqual({ path: ['a'] });
+      });
+
+      it(`should match with input '/a/b'`, () => {
+        const result = pattern.exec('/a/b');
+
+        expect(result).toEqual({ path: ['a', 'b'] });
+      });
+
+      it(`should match with input '/a/b/c'`, () => {
+        const result = pattern.exec('/a/b/c');
+
+        expect(result).toEqual({ path: ['a', 'b', 'c'] });
+      });
+
+      it(`should not match with input '/a/b/'`, () => {
+        const pattern = new PathPattern('/{path}+');
+
+        const result = pattern.exec('/a/b/');
+
+        expect(result).toEqual(null);
+      });
     });
 
-    it(`should match '/{path}+' with input '/a/b'`, () => {
-      const pattern = new PathPattern('/{path}+');
+    describe('/{path}+/', () => {
+      const pattern = new PathPattern('/{path}+/');
 
-      const result = pattern.exec('/a/b');
+      it(`should match with input '/a/b/'`, () => {
+        const result = pattern.exec('/a/b/');
 
-      expect(result).toEqual({ path: ['a', 'b'] });
+        expect(result).toEqual({ path: ['a', 'b'] });
+      });
+
+      it(`should not match with input '/a/b'`, () => {
+        const result = pattern.exec('/a/b');
+
+        expect(result).toEqual(null);
+      });
     });
 
-    it(`should match '/{path}+' with input '/a/b/c'`, () => {
-      const pattern = new PathPattern('/{path}+');
-
-      const result = pattern.exec('/a/b/c');
-
-      expect(result).toEqual({ path: ['a', 'b', 'c'] });
-    });
-
-    it.skip(`should match '/{path}+' with input '/a/b/'`, () => {
-      const pattern = new PathPattern('/{path}+');
-
-      const result = pattern.exec('/a/b/');
-
-      expect(result).toEqual({ path: ['a', 'b', ''] });
-    });
-
-    it(`should match '/{filename}.{ext}' with input '/main.css'`, () => {
+    describe('/{filename}.{ext}', () => {
       const pattern = new PathPattern('/{filename}.{ext}');
 
-      const result = pattern.exec('/main.css');
+      it(`should match with input '/main.css'`, () => {
+        const result = pattern.exec('/main.css');
 
-      expect(result).toEqual({ filename: 'main', ext: 'css' });
+        expect(result).toEqual({ filename: 'main', ext: 'css' });
+      });
+
+      it(`should match with input '/main.abc.css'`, () => {
+        const result = pattern.exec('/main.abc.css');
+
+        expect(result).toEqual({ filename: 'main.abc', ext: 'css' });
+      });
+
+      it(`should not match with input '/.css'`, () => {
+        const result = pattern.exec('/.css');
+
+        expect(result).toEqual(null);
+      });
+
+      it(`should not match with input '/main.'`, () => {
+        const result = pattern.exec('/main.');
+
+        expect(result).toEqual(null);
+      });
     });
 
-    it(`should match '/{filename}.{ext}' with input '/main.abc.css'`, () => {
-      const pattern = new PathPattern('/{filename}.{ext}');
-
-      const result = pattern.exec('/main.abc.css');
-
-      expect(result).toEqual({ filename: 'main.abc', ext: 'css' });
-    });
-
-    it(`should not match '/{filename}.{ext}' with input '/.css'`, () => {
-      const pattern = new PathPattern('/{filename}.{ext}');
-
-      const result = pattern.exec('/.css');
-
-      expect(result).toEqual(null);
-    });
-
-    it(`should not match '/{filename}.{ext}' with input '/main.'`, () => {
-      const pattern = new PathPattern('/{filename}.{ext}');
-
-      const result = pattern.exec('/main.');
-
-      expect(result).toEqual(null);
-    });
-
-    it(`should match '/{filename:[a-z]+}.{ext:css|js}' with input '/main.css'`, () => {
+    describe('/{filename:[a-z]+}.{ext:css|js}', () => {
       const pattern = new PathPattern('/{filename:[a-z]+}.{ext:css|js}');
 
-      const result = pattern.exec('/main.css');
+      it(`should match with input '/main.css'`, () => {
+        const result = pattern.exec('/main.css');
 
-      expect(result).toEqual({ filename: 'main', ext: 'css' });
+        expect(result).toEqual({ filename: 'main', ext: 'css' });
+      });
+
+      it(`should match with input '/main.css'`, () => {
+        const result = pattern.exec('/main.css');
+
+        expect(result).toEqual({ filename: 'main', ext: 'css' });
+      });
+
+      it(`should not match with input '/main.ts'`, () => {
+        const result = pattern.exec('/main.ts');
+
+        expect(result).toEqual(null);
+      });
+
+      it(`should not match with input '/main-chunk.css'`, () => {
+        const result = pattern.exec('/main-chunk.css');
+
+        expect(result).toEqual(null);
+      });
     });
 
-    it(`should not match '/{filename:[a-z]+}.{ext:css|js}' with input '/main.ts'`, () => {
-      const pattern = new PathPattern('/{filename:[a-z]+}.{ext:css|js}');
-
-      const result = pattern.exec('/main.ts');
-
-      expect(result).toEqual(null);
-    });
-
-    it(`should not match '/{filename:[a-z]+}.{ext:css|js}' with input '/main-chunk.css'`, () => {
-      const pattern = new PathPattern('/{filename:[a-z]+}.{ext:css|js}');
-
-      const result = pattern.exec('/main-chunk.css');
-
-      expect(result).toEqual(null);
-    });
-
-    it(`should match '/{year:\\d{4}}-{month:\\d{2}}-{day:\\d{2}}' with input '/2024-09-13'`, () => {
+    describe('/{year:\\d{4}}-{month:\\d{2}}-{day:\\d{2}}', () => {
       const pattern = new PathPattern('/{year:\\d{4}}-{month:\\d{2}}-{day:\\d{2}}');
 
-      const result = pattern.exec('/2024-09-13');
+      it(`should match with input '/2024-09-13'`, () => {
+        const result = pattern.exec('/2024-09-13');
 
-      expect(result).toEqual({ year: '2024', month: '09', day: '13' });
-    });
+        expect(result).toEqual({ year: '2024', month: '09', day: '13' });
+      });
 
-    it(`should not match '/{year:\\d{4}}-{month:\\d{2}}-{day:\\d{2}}' with input '/24-09-13'`, () => {
-      const pattern = new PathPattern('/{year:\\d{4}}-{month:\\d{2}}-{day:\\d{2}}');
+      it(`should not match with input '/24-09-13'`, () => {
+        const result = pattern.exec('/24-09-13');
 
-      const result = pattern.exec('/24-09-13');
-
-      expect(result).toEqual(null);
+        expect(result).toEqual(null);
+      });
     });
 
     it('should extract param with same name', () => {
