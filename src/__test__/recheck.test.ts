9eaadc1d5385444915f3374caaafd68d08ee81b9
stenin-nikita <stenin.nikita@gmail.com>
2024-10-27 18:57:41 +0300

    test: add redos tests

---
+++ src/__test__/recheck.test.ts
@@ -0,0 +1,32 @@
+import { checkSync } from 'recheck';
+import { describe, it, expect } from 'vitest';
+import { PathPattern } from '../path-pattern';
+
+const TEST_CASES = [
+  { routePath: '' },
+  { routePath: '/' },
+  { routePath: '/home' },
+  { routePath: '/home/' },
+  { routePath: '/{param}' },
+  { routePath: '/{param}?' },
+  { routePath: '/users/{id}?' },
+  { routePath: '/users/{id}?/' },
+  { routePath: '/{locale}?/users' },
+  { routePath: '/file/{path}*' },
+  { routePath: '/{path}+' },
+  { routePath: '/{path}+/' },
+  // TODO: redos fix
+  // { routePath: '/{filename}.{ext}' },
+  { routePath: '/{filename:[a-z]+}.{ext:css|js}' },
+  { routePath: '/{year:\\d{4}}-{month:\\d{2}}-{day:\\d{2}}' },
+];
+
+describe('recheck', () => {
+  it.each(TEST_CASES)('$routePath should be safe', ({ routePath }) => {
+    const pattern = new PathPattern(routePath);
+
+    const result = checkSync(pattern.re.source, '');
+
+    expect(result.status).toBe('safe');
+  });
+});
